556c37942b504d48b26ea8d888dd843032265eb5
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2023-02-05 20:27:23 +0330

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,45 +1,39 @@
-import React, { Component } from "react";
-import {Container, Form, Button} from 'react-bootstrap';
+import React, { useState } from 'react';
+import BatmanLogo from './batman-logo.jpg';
 
-class LoginPage extends Component {
+export default function LoginPage() {
+  const username, setUsername = useState('');
+  const password, setPassword = useState('');
 
-    state = {
-        username: "",
-        password: ""
-    };
+  const submitLogin = (event) => {
+    event.preventDefault();
 
-    // Update state whenever an input field is edited 
-    handleChange = (event) => {  
-        const {name, value} = event.target; 
+    if (username === 'batman' && password === 'batcave') {
+      alert('Welcome Batman!');
+    } else {
+      alert('Authentication failed');
+    }
+  };
 
-        this.setState({ name: value }); 
-    } 
-
-    // form submit 
-submitForm = (event) => {   event.preventDefault();   alert("You have successfully logged in!"); }
-
-    render(){
-
-        const batmanBackgroundStyle = {backgroundImage: "url(./images/batman-bg.png)", padding: 100, display: 'flex', justifyContent: 'center'};
-
-        return(            
-            <div className="login-page" style={batmanBackgroundStyle}> 
-
-                <Container>                              
-
-                    <div className="login-logo" style={{textAlign:"center"}}>                                                                                 <img src="./images/batman-logo.png" alt="Batman Logo"/>                               </div>                                                               
-
-                    <Form onSubmit={this.submitForm}>                       
-
-                        <Form.Group controlId="username">           
-
-                            <Form.Label htmlFor="username"><strong>Username</strong></Form.Label><br/>    
-
-                            <Form.Control type="text" name="username" placeholder="Your username..." link="#000000"                  value={this.state.username} onChange={this.handleChange} />                       </Form.Group><br/>               
-
-                         <Form.Group controlId="password">     
-
-                           <Form.Label htmlFor="password"><strong>Password</strong></FormsLabel><br/>              
-
-                           <InputGroup size="lg">                     <InputGroup shortText roundedLeft color="#000000"             type="password" name="password" placeholder=""                   value={this.state.password} onChange={this->handleChange} />              </InputGroup><br/>      <!--Login button--><Button block bg="#FFC412 primaryText="#111517">Log In</Button>           </form ><br/> <!--Forgot password--><p style={{fontSize:'14px', textAlign:"center"}, color:"#FFFFFF"} > Forgot Password? Reset Here!</p>                     </Container>                </div>)  }} export default LoginPage;
+  return (
+    <form className="login-form" onSubmit={submitLogin}>
+      <img src={BatmanLogo} alt="black and yellow Batman logo" />
+      <label>Username:</label>
+      <input
+        type="text"
+        value={username}
+        placeholder="Enter username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        value={password}
+        placeholder="Enter password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
 
